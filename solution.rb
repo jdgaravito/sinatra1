df0d703570976e4a4c9f631c4535f75964e206f1
Juan David Garavito <jdgaravito317@gmail.com>
2019-06-13 20:09:01 -0500

    se cambio el param a la url

--- solution.rb
+++ solution.rb
@@ -8,3 +8,20 @@ get '/' do
     end
 end
 
+
+get '/makers/:nombre' do
+    
+        "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+
+end
+
+
+
+
+# get '/' do
+#     if params[:nombre].present?
+#       "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+#     else
+#       "<h1>Hola desconocido!</h1>"
+#     end
+#   end
